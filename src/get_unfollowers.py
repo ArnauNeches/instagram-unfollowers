f0f59b0302a83d9c0d231bb0bbf5a15a9343d15e
ArnauNeches <arnichi2103@gmail.com>
2025-12-23 14:16:00 +0100

    prettyprint

--- src/get_unfollowers.py
+++ src/get_unfollowers.py
@@ -5,9 +5,12 @@ import json
 
 load_dotenv()
 
+followers = set()
+followed = set()
+
 URL = f"https://www.instagram.com/api/v1/friendships/{os.environ["id"]}/followers/"
 
-params = {"count": 12, "search_surface": "follow_list_page"}
+params = {"count": 25, "search_surface": "follow_list_page"} # Apparently 25 is the limit, on my browser it does 12
 
 cookie_list = ["crsftoken", "datr", "dpr", "ds_user_id", "ig_did", "mid", "rur", "sessionid", "wd"]
 cookie_dict = {cookie:os.environ[cookie] for cookie in cookie_list}
@@ -18,7 +21,11 @@ header_dict = {header:os.environ[header] for header in header_list}
 response = requests.get(URL, params=params, cookies=cookie_dict, headers=header_dict)
 
 print(response.status_code)
-print(response.text)
+parsed = json.loads(response.text)
+names = [user["username"] for user in parsed["users"]]
+print(names)
 
 
 
+# End of program
+# Print followed - followers
