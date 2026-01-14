f0a040be55f565552e784ac7461a13a507a56f66
ArnauNeches <arnichi2103@gmail.com>
2026-01-14 17:04:59 +0100

    started migration into chrome extension

---
+++ src/popup.js
@@ -0,0 +1,6 @@
+document.getElementById("analyze").addEventListener("click", onButtonClick);
+document.getElementById("lists").addEventListener("click", onButtonClick);
+
+async function onButtonClick(){
+    console.log("Clicked button")
+}
